e3d310775dbeaa2709522e91272bb6e13aa29764
Alex Harasty <alex.harasty@thinkingwithtools.com>
2019-07-27 13:57:10 -0400

    initial header

--- client/texting/src/app/app.module.ts
+++ client/texting/src/app/app.module.ts
@@ -1,15 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TextViewComponent } from './text-view/text-view.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TextViewComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
