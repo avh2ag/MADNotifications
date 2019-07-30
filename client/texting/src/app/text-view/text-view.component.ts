51a707b562e222c902d19e3a044e650afdd56e1a
Alex Harasty <alex.harasty@thinkingwithtools.com>
2019-07-30 12:13:15 -0400

    get the notifications right on submission

--- client/texting/src/app/text-view/text-view.component.ts
+++ client/texting/src/app/text-view/text-view.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { TextingService } from '../texting.service';
+import { each } from 'lodash';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-text-view',
   templateUrl: './text-view.component.html',
   styleUrls: ['./text-view.component.css']
 })
-export class TextViewComponent implements OnInit {
+export class TextViewComponent implements OnInit, OnDestroy {
   textForm: FormGroup;
-  isSubmitted  =  false;
+  isSubmitted = false;
+  subscriptions = {};
+  messageStatuses: object = {};
+  numbersList: Array<string> = [];
   constructor(private formBuilder: FormBuilder, private texting: TextingService) { }
 
   ngOnInit() {
@@ -17,18 +22,73 @@ export class TextViewComponent implements OnInit {
       message: ['', Validators.required],
       numbers: ['', Validators.required]
     });
-    console.log(this.texting);
+    this._registerSubscriptions();
+  }
+
+  ngOnDestroy() {
+    this._deregisterSubscriptions();
   }
 
   get formControls() { return this.textForm.controls; }
 
+  public resetForm() {
+    this.textForm.reset();
+    this.isSubmitted = false;
+  }
+
+  public resetStatuses() {
+    this.messageStatuses = {};
+    this.numbersList = [];
+  }
+
+  public generateNumbersList() {
+    this.numbersList = this.textForm.get('numbers').value.split(',');
+    this.numbersList = this.numbersList.map((rawNumber: string) => {
+      return this.texting.convertTelephoneNumber(rawNumber);
+    }).filter((convertedNumber: string) => {
+      return convertedNumber.length > 0;
+    });
+  }
+
+  public populateStatuses() {
+    this.numbersList.forEach((validNumber: string) => {
+      this.messageStatuses[validNumber] = 'Pending';
+    });
+  }
+
   public sendMessages() {
     console.log('sending messages');
     if (this.textForm.valid) {
+      this.isSubmitted = true;
+      this.resetStatuses();
+      this.generateNumbersList();
+      this.populateStatuses();
+      const message = this.textForm.get('message').value;
+      this.numbersList.forEach((phoneNumber: string) => {
+        this.texting.sendMessage(phoneNumber, message);
+      });
       // split the strings by comma
       // put in format +1123456789
       // send to aws
     }
+  }
+
+  public updatePendingMessageStatus( status: { phoneNumber: string, status: string } ) {
+    this.messageStatuses[status.phoneNumber] = status.status;
+  }
+
+  private _registerSubscriptions() {
+    this.subscriptions[this.texting.statusSubjectName] =
+      this.texting.getSubject(this.texting.statusSubjectName)
+        .subscribe( status => {
+          this.updatePendingMessageStatus(status);
+        });
+  }
+
+  private _deregisterSubscriptions() {
+    each(this.subscriptions, (sub: Subscription, key: string) => {
+      sub.unsubscribe();
+    });
   }
 
 }
--- client/texting/src/app/texting.service.ts
+++ client/texting/src/app/texting.service.ts
@@ -6,15 +6,45 @@ import { Subject } from 'rxjs';
 })
 export class TextingService {
   private subjectMap = {};
-  public errorSubjectName = 'error';
-  public successSubectName = 'success';
+  public statusSubjectName = 'status';
   constructor(private http: HttpClient) {
-    this.subjectMap[this.errorSubjectName] = new Subject<any>();
-    this.subjectMap[this.successSubectName] = new Subject<any>();
+    this.subjectMap[this.statusSubjectName] = new Subject<any>();
   }
 
   public getSubject(subjectName: string) {
     return this.subjectMap[subjectName].asObservable();
+  }
+
+  // needs to be in +1<areacode><number>
+  public convertTelephoneNumber(rawTelNumber: string): string {
+    rawTelNumber = rawTelNumber.replace(/\D/g, '');
+    // must enforce 10 digit number
+    if (rawTelNumber.length !== 10) {
+      // make the blank string invalid
+      return '';
+    } else {
+      return `+1${rawTelNumber}`;
+    }
+  }
+
+  private sendStatus(phoneNumber: string, status: string) {
+    const statusObj = {
+      phoneNumber: phoneNumber,
+      status: status
+    };
+    this.subjectMap[this.statusSubjectName].next(statusObj);
+  }
+
+
+  public sendMessage(phoneNumber: string, message: string) {
+    if (!phoneNumber || phoneNumber.length === 0) {
+      return;
+    } else {
+      // add the +1 to the phoneNumber
+      const formattedPhoneNumber = `+1${phoneNumber}`;
+      // send request
+      this.sendStatus(phoneNumber, 'Error');
+    }
   }
 
 }
