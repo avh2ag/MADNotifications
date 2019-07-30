034b299bbf05c79c9c43156afe2422c709257d45
Alex Harasty <alex.harasty@thinkingwithtools.com>
2019-07-29 21:12:11 -0400

    simple reactive form and submit binding

--- client/texting/src/app/text-view/text-view.component.ts
+++ client/texting/src/app/text-view/text-view.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-text-view',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./text-view.component.css']
 })
 export class TextViewComponent implements OnInit {
-
-  constructor() { }
+  textForm: FormGroup;
+  isSubmitted  =  false;
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.textForm  =  this.formBuilder.group({
+      message: ['', Validators.required],
+      numbers: ['', Validators.required]
+    });
+  }
+
+  get formControls() { return this.textForm.controls; }
+
+  public sendMessages() {
+    console.log('sending messages');
+    if (this.textForm.valid) {
+      // split the strings by comma
+      // put in format +1123456789
+      // send to aws
+    }
   }
 
 }
